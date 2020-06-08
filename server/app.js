26a05728249b8dfdf327ee83cef01c605466dbb1
Touqeer <touqeerhassan76@gmail.com>
2020-06-09 00:53:10 +0500

    initial

--- server/app.js
+++ server/app.js
@@ -13,7 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
+app.use('/api/', indexRouter);
+app.use('/', express.static('build'));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
